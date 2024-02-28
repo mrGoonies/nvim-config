e0ee3b4ce6c1cb09ceafac15b0720b67637aaa46
mrGoonies <gooniesdev@gmail.com>
2024-02-28 17:31:06 -0300

    Añadiendo LazyNvim como Plugin Manager

--- init.lua
+++ init.lua
@@ -5,3 +5,19 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.cmd("set relativenumber")
+
+-- === LazyNvim ===
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins, opts)
