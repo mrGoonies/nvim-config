9949d207fa276ff64544d3461d02b945e7dde0f0
mrGoonies <gooniesdev@gmail.com>
2024-03-05 19:29:52 -0300

    Iconos y treebar para archivos del proyecto

--- init.lua
+++ init.lua
@@ -32,6 +32,16 @@ local plugins = {
     build = ":TSUpdate"
   },
   -- IDE Settings
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  },
+  {"nvim-tree/nvim-web-devicons", name = "nvim-web-devicons"}
 }
 
 local opts = {}
